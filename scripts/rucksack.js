f484de7daf539b2339d98ee637a07f598cb6cbe6
Joe Mottershaw <joemottershaw.hello@gmail.com>
2018-07-25 13:40:02 +0100

    Fix click event helper handlers

--- scripts/rucksack.js
+++ scripts/rucksack.js
@@ -12,17 +12,34 @@ document.documentElement.className.replace('no-js', 'js');
    RUCKSACK -> CLICK EVENT HELPER HANDLERS
    ============================================================================================= */
 
-// Prevent default
-document.querySelector('.prevent-default').addEventListener('click', (event) => {
-    event.preventDefault();
+// Set prevent defeaul, immediate propagation and stop immediate propagation elements
+const preventDefaultElements = document.querySelectorAll('.prevent-default');
+const stopPropagationElements = document.querySelectorAll('.stop-propagation');
+const stopImmediatePropagationElements = document.querySelectorAll('.stop-immediate-propagation');
+
+// Loop through all the prevent default elements
+Array.from(preventDefaultElements).forEach(element => {
+    // Add a click event handler to the element
+    element.addEventListener('click', (event) => {
+        // Prevent default
+        event.preventDefault();
+    });
 });
 
-// Stop propagation
-document.querySelector('.stop-propagation').addEventListener('click', (event) => {
-    event.stopPropagation();
+// Loop through all the stop propagation elements
+Array.from(stopPropagationElements).forEach(element => {
+    // Add a click event handler to the element
+    element.addEventListener('click', (event) => {
+        // Stop propagation
+        event.stopPropagation();
+    });
 });
 
-// Stop immediate propagation
-document.querySelector('.stop-immediate-propagation').addEventListener('click', (event) => {
-    event.stopImmediatePropagation();
+// Loop through all the stop immediate propagation elements
+Array.from(stopImmediatePropagationElements).forEach(element => {
+    // Add a click event handler to the element
+    element.addEventListener('click', (event) => {
+        // Stop immediate propagation
+        event.stopImmediatePropagation();
+    });
 });
