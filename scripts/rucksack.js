a757f7a4f6d1dec67477c35f658ce6f3a59a90fb
Joe Mottershaw <joemottershaw.hello@gmail.com>
2018-07-25 11:05:50 +0100

    Add helpful click event handlers

--- scripts/rucksack.js
+++ scripts/rucksack.js
@@ -6,3 +6,23 @@
 
 // Javascript
 document.documentElement.className.replace('no-js', 'js');
+
+
+/* =============================================================================================
+   RUCKSACK -> CLICK EVENT HELPER HANDLERS
+   ============================================================================================= */
+
+// Prevent default
+document.querySelector(".prevent-default").addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+// Stop propagation
+document.querySelector('.stop-propagation').addEventListener('click', (event) => {
+    event.stopPropagation();
+});
+
+// Stop immediate propagation
+document.querySelector('.stop-immediate-propagation').addEventListener('click', (event) => {
+    event.stopImmediatePropagation();
+});
