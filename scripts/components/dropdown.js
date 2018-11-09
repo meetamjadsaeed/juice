1c4d8768c208ba84f793156ff56a44ba52c09b06
Joe Mottershaw <joemottershaw.hello@gmail.com>
2018-11-09 15:32:28 +0000

    Update dropdown component and navigation element
    Update dropdown component and navigation element
    
    - Add dropdown is hoverable modifier class
    - Add dropdown megamenu modifier class
    - Add dropdown container element for offsets
    - Add dropdown width values
    - Update dropdown default animation duration
    - Update dropdown default offset amount
    - Remove dropdown injected tabindex attribute
    - Remove dropdown min width values
    - Remove navigation megamenu variables and styles
    - Remove navigation sub nav variables and styles

--- scripts/components/dropdown.js
+++ scripts/components/dropdown.js
@@ -11,7 +11,7 @@
     // Set the plugin defaults
     const defaults = {
         animationIn: 'fadeInUpTiny',
-        animationOut: 'fadeOutDownTiny',
+        animationOut: 'fadeOutDownTiny'
     };
 
     /**
@@ -40,30 +40,63 @@
         initialize() {
             // Set the target element
             const $element = $(this.element);
-            const $dropdown = $('.dropdown', $element);
-
-            // Add a tabindex attribute to the element
-            $element.attr('tabindex', -1);
+            const $dropdown = $element.find('.dropdown');
+            const $dropdownContent = $('.dropdown__content', $dropdown);
 
             // Store the settings to the dropdown data
             $dropdown.data({
                 'animation-in': $element.data('animation-in') || this.settings.animationIn,
-                'animation-out': $element.data('animation-out') || this.settings.animationOut,
+                'animation-out': $element.data('animation-out') || this.settings.animationOut
             });
 
-            // Check if the dropdown has the center modifier class
-            if ($dropdown.hasClass('dropdown--center')) {
-                // Set the dropdown width
-                const dropdownWidth = $dropdown.outerWidth();
+            // Check if the dropdown does not have the megamenu modifier class
+            if (!$dropdown.hasClass('dropdown--megamenu')) {
+                // Check if the dropdown has the center modifier class
+                if ($dropdown.hasClass('dropdown--center')) {
+                    // Set the element width
+                    const elementWidth = $element.outerWidth();
 
-                // Set the left margin inline style of the dropdown
-                $dropdown.css({
-                    'margin-left': -(dropdownWidth / 2)
+                    // Set the left margin inline style of the dropdown
+                    $dropdown.css({
+                        'margin-left': elementWidth / 2
+                    });
+                }
+
+                // Check if the dropdown has the right modifier class
+                if ($dropdown.hasClass('dropdown--right')) {
+                    // Set the element width
+                    const elementWidth = $element.outerWidth();
+
+                    // Set the left margin inline style of the dropdown
+                    $dropdown.css({
+                        'margin-left': elementWidth
+                    });
+                }
+            }
+
+            // Check if the dropdown has the is hoverable state class and the html tag doesn't have the touch detection class
+            if ($dropdown.hasClass('is-hoverable') && !$('html').hasClass('touch')) {
+                // Add a mouse enter event handler on the dropdown container
+                $element.on('mouseenter', (event) => {
+                    // Set the dropdown element
+                    const $dropdown = $(event.currentTarget).find('.dropdown');
+
+                    // Show the dropdown
+                    this.show($dropdown);
+                });
+
+                // Add a mouse leave event handler on the dropdown container
+                $element.on('mouseleave', (event) => {
+                    // Set the dropdown element
+                    const $dropdown = $(event.currentTarget).find('.dropdown');
+
+                    // Hide the dropdown
+                    this.hide($dropdown);
                 });
             }
 
-            // Add a click handler to show and hide the dropdown
-            $element.off().on('click', '.js-dropdown-trigger', (event) => {
+            // Add a click event handler to show and hide the dropdown
+            $element.on('click', '.js-dropdown-trigger', (event) => {
                 // Set the dropdown element
                 const $dropdown = $(event.currentTarget).next('.dropdown');
 
@@ -77,16 +110,7 @@
                 }
             });
 
-            // Add a focus event handler on the dropdown container to focus on the dropdown trigger
-            $element.on('focus', (event) => {
-                // Set the dropdown trigger element
-                const $trigger = $(event.currentTarget).children('.js-dropdown-trigger');
-
-                // Focus on the trigger element
-                $trigger.focus();
-            });
-
-            // Add a focusout handler to hide the dropdown
+            // Add a focus out event handler to hide the dropdown
             $element.on('focusout', (event) => {
                 // Set the current target, related target and dropdown elements
                 const $current = $(event.currentTarget);
@@ -107,13 +131,16 @@
          * @return {void}
          */
         show($dropdown) {
-            // Toggle the is active state hook class on the dropdown
-            $dropdown.toggleClass('is-active');
+            // Set the dropdown content element
+            const $dropdownContent = $('.dropdown__content', $dropdown);
 
-            // Add the animation in class to the dropdown and check when the animation has ended
-            $dropdown.addClass($dropdown.data('animation-in')).one('animationend', () => {
-                // Remove the animation in class
-                $dropdown.removeClass($dropdown.data('animation-in'));
+            // Toggle the is active state hook class on the dropdown
+            $dropdown.addClass('is-active');
+
+            // Add the animation in class to the dropdown content and check when the animation has ended
+            $dropdownContent.addClass($dropdown.data('animation-in')).one('animationend', () => {
+                // Remove the animation in class from the dropdown content
+                $dropdownContent.removeClass($dropdown.data('animation-in'));
             });
         },
 
@@ -123,10 +150,13 @@
          * @return {void}
          */
         hide($dropdown) {
-            // Add the animation out class to the dropdown and check when the animation has ended
-            $dropdown.addClass($dropdown.data('animation-out')).one('animationend', () => {
-                // Remove the animation out class
-                $dropdown.removeClass($dropdown.data('animation-out'));
+            // Set the dropdown content element
+            const $dropdownContent = $('.dropdown__content', $dropdown);
+
+            // Add the animation out class to the dropdown content and check when the animation has ended
+            $dropdownContent.addClass($dropdown.data('animation-out')).one('animationend', () => {
+                // Remove the animation out class from the dropdown content
+                $dropdownContent.removeClass($dropdown.data('animation-out'));
 
                 // Remove the is active state hook class on the dropdown
                 $dropdown.removeClass('is-active');
